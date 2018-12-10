08023e30573306c136d2b9d319ce1fc2bc7e40f0
Eunji Bae <emilymilybae@gmail.com>
2018-12-10 15:29:52 -0800

    12/10

--- js/work-mantra.js
+++ js/work-mantra.js
@@ -105,3 +105,12 @@ function test(element) {
         var chart = new google.visualization.PieChart(document.getElementById('Time_chart_div'));
         chart.draw(data, options);
       }
+$(window).on("throttledresize", function (event) {
+    var options = {
+        width: '100%',
+        height: '100%'
+    };
+
+    var data = google.visualization.arrayToDataTable([]);
+    drawChart(data, options);
+});
